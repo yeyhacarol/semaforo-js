af64c5117dd28d3faf26f71e59b1e6853aec18f0
CAROLINA VITÓRIA CHAVES SILVA <21276093@CFP127ALUNO.LOCAL>
2022-02-16 13:52:37 -0300

    semáforo funcionando

---
+++ main.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const semaforo = document.getElementById('semaforo')
+const vermelho = document.getElementById('vermelho')
+const amarelo = document.getElementById('amarelo')
+const verde = document.getElementById('verde')
+const automatico = document.getElementById('automatico')
+let idFarol = null
+let farol = 1
+
+const luzVermelha = () => semaforo.src = './img/vermelho.png'
+
+const luzAmarela = () => semaforo.src = './img/amarelo.png'
+
+const luzVerde = () => semaforo.src = './img/verde.png'
+
+const automaticoLigado = () => idFarol != null
+
+const desligarAutomatico = () => {
+    if (automaticoLigado()) {
+        clearInterval(idFarol)
+        idFarol = null
+    }
+} 
+
+const luzesAutomaticas = () => {
+    if (farol == 1) {
+        luzVerde()
+        farol++
+    } else if (farol == 2) {
+        luzAmarela()
+        farol++
+    } else if (farol == 3) {
+        luzVermelha()
+        farol++
+    } else if (farol == 4) {
+        luzAmarela()
+        farol = 1
+    }
+}
+
+const trocarLuzes = () => {
+   if (idFarol == null) {
+       idFarol = setInterval(luzesAutomaticas, 900)
+   } else {
+       clearInterval(idFarol)
+       idFarol = null
+   } 
+}
+
+
+vermelho.addEventListener('click', () => {
+    desligarAutomatico()
+    farol = 1
+    luzVermelha()
+})
+
+amarelo.addEventListener('click', () => {
+    desligarAutomatico()
+    farol = 2
+    luzAmarela()
+})
+
+verde.addEventListener('click', () => {
+    desligarAutomatico()
+    farol = 3
+    luzVerde()
+})
+
+automatico.addEventListener('click', trocarLuzes)
